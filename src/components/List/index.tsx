0dbbe7cebc41d26e5df6ab8a1b96819d30d61018
Dev-W1ll <93744707+Dev-W1ll@users.noreply.github.com>
2024-09-10 00:20:42 -0300

    Initial commit

---
+++ src/components/List/index.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import style from './List.module.scss';
+
+function List() {
+    const tasks = [{
+        task: 'React',
+        time: '02:00:00'
+    }, {
+        task: 'JavaScript',
+        time: '01:00:00'
+    }, {
+        task: 'TypeScript',
+        time: '03:00:00'
+    }]
+    return (
+        <aside className={style.listTasks}>
+            <h2>Estudos do dia</h2>
+            <ul>
+                {tasks.map((item, index) => (
+                    <li key={index} className={style.item}>
+                        <h3> {item.task} </h3>
+                        <span> {item.time} </span>
+                    </li>
+                ))}
+            </ul>
+        </aside>
+    )
+}
+
+export default List;
