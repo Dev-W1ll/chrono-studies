697d7e1a0480dd22824ac84bfd3b728543c727dd
Dev-W1ll <93744707+Dev-W1ll@users.noreply.github.com>
2024-10-06 01:13:43 -0300

    feat: Add task state management, typing, and prop integration in Form and List components.

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import style from './Button.module.scss';
 
-class Button extends React.Component<{ children: any }> {
+class Button extends React.Component<{
+    type?: "button" | "submit" | "reset" | undefined, children: React.ReactNode,
+}> {
     render() {
+        const { type = "button" } = this.props;
         return (
-            <button className={style.button}>
+            <button type={type} className={style.button}>
                 {this.props.children}
             </button>
         )
--- src/components/List/index.tsx
+++ src/components/List/index.tsx
@@ -1,23 +1,11 @@
-import React, { useState } from "react";
-import style from './List.module.scss';
+import { ITask } from "../../types/task";
 import Item from "./Item";
+import style from './List.module.scss';
 
-function List() {
-    const [tasks, setTasks] = useState([{
-        task: 'React',
-        time: '02:00:00'
-    }, {
-        task: 'JavaScript',
-        time: '01:00:00'
-    }, {
-        task: 'TypeScript',
-        time: '03:00:00'
-    }]);
+function List({ tasks }: { tasks: ITask[] }) {
     return (
         <aside className={style.listTasks}>
-            <h2 onClick={() => {
-                setTasks([...tasks, { task: "Estudar estado", time: "05:00:00" }])
-            }}>Estudos do dia</h2>
+            <h2>Estudos do dia</h2>
             <ul>
                 {tasks.map((item, index) => (
                     <Item
